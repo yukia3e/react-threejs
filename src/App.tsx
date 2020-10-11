1d4f910f020cd21ea7f92afa75d75e112d758f30
Yuki Abe <12268087+yukia3@users.noreply.github.com>
2020-10-11 19:54:25 +0900

    Use React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React, { FC } from 'react';
-import { Canvas } from 'react-three-fiber';
-import Controls from 'components/Controls';
-import Box from 'components/Box';
+import { Switch, Route } from 'react-router-dom';
+import Home from 'pages/Home';
 
 const App: FC = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <Controls isControl />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 };
 
